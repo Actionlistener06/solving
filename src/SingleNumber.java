2af6352f95231ad755071204e8438f0ea0890fd4
mustafa <mserhatdemirtas@gmail.com>
2023-02-03 20:28:34 +0300

    n

---
+++ src/SingleNumber.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        System.out.println(new SingleNumber().singleNumber( new int[]{4, 1, 2, 1, 2}));
+    }
+
+    public int singleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i=i+2){
+            if (nums[i] != nums[i +1]  ) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+
+
+    }
+}
